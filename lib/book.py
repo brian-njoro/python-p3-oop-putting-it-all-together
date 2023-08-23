e804864e9bece22be747029ee43778b70b0fe303
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-23 13:52:02 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def check_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            return page_count
+        else :
+            raise ValueError("page count must be an integer")
+    _page_count = property(check_count,set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, shoe_size):
+        self.brand = brand
+        self._size = shoe_size
+
+    def check_size(self):
+        return self._size
+
+    def set_size(self, shoe_size):
+        if isinstance (shoe_size, int):
+            return shoe_size
+        else:
+            raise ValueError("shoe size must bean integer") 
+        
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as mew")
+    
+    _size = property(check_size, set_size)
